4b88eff491d9c76bc26d7517f54b8c411fb51dee
sterling-gore <155609125+Sterling-Gore@users.noreply.github.com.>
2025-01-25 11:01:05 -0600

    bubble doesn't spawn on player, but instead spawns around the player. Bubble won't move backwards towards the player if you click on the player, it will instead freeze on it's spawn point

--- BubblegumProject/Assets/scripts/shoot_Bubble.cs
+++ BubblegumProject/Assets/scripts/shoot_Bubble.cs
@@ -7,6 +7,7 @@ public class shoot_Bubble : MonoBehaviour
     [SerializeField] private Transform BubbleTemplate;
     [SerializeField] private Transform BubbleContainer;
     [SerializeField] private Camera cam;
+    [SerializeField] private float Bubble_Spawn_Distance = 1.2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,16 +23,37 @@ public class shoot_Bubble : MonoBehaviour
             NewBubble.gameObject.SetActive(true);
             Vector3 mousePos = Input.mousePosition;
             Vector3 point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
-            NewBubble.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+
+            Vector2 destinationPoint = new Vector2(point.x, point.y);
+            Vector2 playerPosition = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+            Vector2 start_to_destination_vector = new Vector2(point.x - playerPosition.x, point.y - playerPosition.y) ;
+            Vector2 bubbleSpawnPosition = new Vector2(playerPosition.x + start_to_destination_vector.normalized.x * Bubble_Spawn_Distance, playerPosition.y + start_to_destination_vector.normalized.y * Bubble_Spawn_Distance);
+
+
+            //NewBubble.position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+            NewBubble.position = bubbleSpawnPosition;
 
             bubble_movement bubble_script = NewBubble.GetComponent<bubble_movement>();
-            bubble_script.destination = new Vector2(point.x, point.y);
+            if(VectorIsBetweenPlayerAndBubbleSpawn(playerPosition, bubbleSpawnPosition, destinationPoint))
+            {
+                //if your cursor is inside the radius between where the bubble spawn and where the player is, the bubble will then not move from its spawn and its destination will be its spawn point
+                bubble_script.destination = bubbleSpawnPosition;
+            }
+            else
+            {
+                bubble_script.destination = new Vector2(point.x, point.y);
+            }
             bubble_script.start = true;
 
 
-            //transform.position = Vector2.Lerp(transform.position, destination, Time.deltaTime);
+            //+transform.position = Vector2.Lerp(transform.position, destination, Time.deltaTime);
     
         }
+    }
+
+    bool VectorIsBetweenPlayerAndBubbleSpawn(Vector2 PlayerPos, Vector2 BubbleSpawnPos, Vector2 DestinationPos)
+    {
+        return (Vector2.Dot( (BubbleSpawnPos - PlayerPos).normalized, (DestinationPos- BubbleSpawnPos).normalized) <0f && Vector2.Dot( (PlayerPos-BubbleSpawnPos).normalized,  (DestinationPos-PlayerPos).normalized) <0f);
     }
 
 }
