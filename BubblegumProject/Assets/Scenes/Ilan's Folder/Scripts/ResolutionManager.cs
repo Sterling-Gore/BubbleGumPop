b2abd782b11800e20afdb43ca3cf428f656cc80d
Ilan Lipsky <102115093+CoinismRS@users.noreply.github.com>
2025-01-25 18:59:25 -0600

    Ilan_MenuUI
    -Implemented start menu
    -Implemented options menu
    -Implemented basic scene transition mechanics
    -Implemented button UI for start menu, option menu

---
+++ BubblegumProject/Assets/Scenes/Ilan's Folder/Scripts/ResolutionManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // For TextMeshPro dropdown
+
+public class ResolutionManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+
+    private Resolution[] resolutions;
+
+    void Start()
+    {
+        // Get available screen resolutions
+        resolutions = Screen.resolutions;
+
+        // Clear existing options in the dropdown
+        resolutionDropdown.ClearOptions();
+
+        // Create a list of resolution options as strings
+        var options = new System.Collections.Generic.List<string>();
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            // Check if this resolution is the current one
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        // Add options to the dropdown
+        resolutionDropdown.AddOptions(options);
+
+        // Set the current resolution as the selected option
+        resolutionDropdown.value = currentResolutionIndex;
+        //resolutionDropdown.RefreshShownValue();
+
+        // Add a listener for when the value changes
+        resolutionDropdown.onValueChanged.AddListener(SetResolution);
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution selectedResolution = resolutions[resolutionIndex];
+        Screen.SetResolution(selectedResolution.width, selectedResolution.height, Screen.fullScreenMode);
+    }
+}
+
+
+
